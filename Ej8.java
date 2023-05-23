f536fea07e27005ecbf63774185bb3c0c907077b
JoaquinGalarza <86010277+JoaquinGalarza@users.noreply.github.com>
2023-05-22 21:53:12 -0300

    Add files via upload
    Ejercicio 8

---
+++ Ej8.java
@@ -0,0 +1,11 @@
+public class Ej8 {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        persona1.setEdad(21);
+        persona1.setNombre("Choerry");
+        persona1.setTelefono("08");
+        System.out.println("La edad de la persona es: "+persona1.getEdad());
+        System.out.println("El nombre de la persona es: "+persona1.getNombre());
+        System.out.println("EL telefono de la persona es: "+persona1.getTelefono());
+    }
+}
